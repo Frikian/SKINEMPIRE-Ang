94b76565a1cc83619dc16ddcb6bb797cc9d8fd41
kituss44 <marc.romaguera@institutvidreres.cat>
2026-01-19 12:12:20 +0100

    rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,28 @@
 import { Routes } from '@angular/router';
 import { Carrito } from  './carrito/carrito';
 import {Index} from './index';
+import {Contacto} from './contacto/contacto';
+import {Hdtw} from './hdtw/hdtw';
+import {Login} from './login/login';
+import {PoliticaDeReembolso} from './politica-de-reembolso/politica-de-reembolso';
+import {PreguntasFrecuentes} from './preguntas-frecuentes/preguntas-frecuentes';
+import {Register} from './register/register';
+import {TerminosYCondiciones} from './terminos-y-condiciones/terminos-y-condiciones';
+import {Tienda} from './tienda/tienda';
+import {Venta} from './venta/venta';
 
 
 export const routes: Routes = [
   {path: 'carrito', component: Carrito},
   {path: '', component: Index},
-
+  {path: 'contacto', component: Contacto},
+  {path: 'hdtw', component: Hdtw},
+  {path: 'index', component: Index},
+  {path: 'ligin', component: Login},
+  {path: 'politica-de-reembolso', component: PoliticaDeReembolso},
+  {path: 'preguntas-frecuentes', component: PreguntasFrecuentes},
+  {path: 'register', component: Register},
+  {path: 'terminos-y-condiciones', component: TerminosYCondiciones},
+  {path: 'tienda', component: Tienda},
+  {path: 'venta', component: Venta}
 ];
