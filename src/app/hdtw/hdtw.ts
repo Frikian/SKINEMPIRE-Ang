4e5289a163a0faf8c7ceb378deea363e6df4aaf0
Frikian <ianfh271205@gmail.com>
2026-01-22 09:29:10 +0100

    Fotos i css font

--- src/app/hdtw/hdtw.ts
+++ src/app/hdtw/hdtw.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-hdtw',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './hdtw.html',
   styleUrl: './hdtw.css',
 })
