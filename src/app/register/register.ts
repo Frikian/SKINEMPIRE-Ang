fd4cf9389f08c2c7c52a076e2983d0479f819f4e
kituss44 <marc.romaguera@institutvidreres.cat>
2026-01-23 11:44:36 +0100

    inici serveis

--- src/app/register/register.ts
+++ src/app/register/register.ts
@@ -1,28 +1,14 @@
 import { Component } from '@angular/core';
-import {Router, RouterLink} from '@angular/router';
-import {FormsModule} from '@angular/forms';
+import {Usuaris} from '../serveis/usuaris';
 
 @Component({
   selector: 'app-register',
-  imports: [
-    RouterLink,
-    FormsModule
-  ],
+  imports: [],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
 export class Register {
-  user = {
-    usuari: "",
-    correu: "",
-    contrasenya: ""
-  }
-
-  constructor(private router: Router) {}
-
-  onRegister() {
-    sessionStorage.setItem('user', JSON.stringify(this.user));
-    alert('Registre completat correctament!');
-    this.router.navigate(['/login']);
+  nom: string= ""
+  constructor(private Usuarisss: Usuaris) {
   }
 }
